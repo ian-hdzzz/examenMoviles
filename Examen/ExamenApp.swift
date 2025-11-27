fafa6e110f0cd40bf503465c91276662edf12d3a
Ian <ianhdez2020@gmail.com>
2025-11-27 15:24:49 -0600

    first commit

---
+++ Examen/ExamenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamenApp.swift
+//  Examen
+//
+//  Created by Ian Hernandez on 27/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ExamenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
